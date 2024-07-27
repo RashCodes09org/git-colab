83975a4345d7322b07222dba60b686f0dcfcfe19
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-07-27 14:58:35 +0100

    rasheedat

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div>This is header page</div>;
+  return <div>This is header page, to see if rasheedat is working</div>;
 };
 
 export default Header;
